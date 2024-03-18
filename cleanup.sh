da04b1ddb688716ec02c2ae1321e9cfc83ed0054
yishaigalili <yishai9300@gmail.com>
2024-03-18 08:27:01 -0400

    tests works

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,9 @@
 # Delete the db
 cleanup() {
     echo "Cleaning up..."
-    sudo rm -rf "/home/$USER/Desktop/disko/src/__pycache__/" 
-    sudo rm -rf "/home/$USER/Desktop/disko/src/disko/__pycache__/"
-    sudo rm -rf "/home/$USER/Desktop/disko/src/mvc/__pycache__/"
+    sudo rm -rf "./src/__pycache__/" 
+    sudo rm -rf "./src/disko/__pycache__/"
+    sudo rm -rf "./src/mvc/__pycache__/"
     sudo rm -rf image_data.db
     sleep 1
     echo "Cleaned up!" 
