772eb04ff034b4f98c625f37af1e8542d2c81c4f
Daniel Shwartzman <dshwartzman5@gmail.com>
2024-03-21 13:14:08 +0200

    changed db structure

--- src/disko/image_collector.py
+++ src/disko/image_collector.py
@@ -11,12 +11,9 @@ class ImageCollector:
         config.load_kube_config()
 
         # Create a connection to the SQLite database
-        # self.conn = sqlite3.connect("image_data.db")
-        # self.cursor = self.conn.cursor()
-        # self.cursor.execute("""CREATE TABLE IF NOT EXISTS images (image_name TEXT)""")
-        
         self.crud = SQLiteCRUD(self.dbfilename)
-        self.crud.create_table("images", ["image_name TEXT", "timestamp TIMESTAMP"], "image_name, timestamp")
+        # Create the images table if it does not exist
+        self.crud.create_table("images", ["cluster_name TEXT, image_name TEXT", "timestamp TIMESTAMP, registry TEXT, amount INTEGER"], "cluster_name, image_name, timestamp")
 
         # Create the Kubernetes API client
         self.v1 = client.CoreV1Api()
@@ -25,28 +22,19 @@ class ImageCollector:
         # Get the list of namespaces
         namespaces = [ns.metadata.name for ns in self.v1.list_namespace().items]
 
-        # Open the file in write mode and clear its contents
-        # with open(self.filename, "w") as f:
-        #     f.truncate(0)
-
         # Iterate through the namespaces
         for ns in namespaces:
             # Get the list of pods in the namespace
             pods = self.v1.list_namespaced_pod(namespace=ns).items
-
             # Iterate through the pods
             for pod in pods:
                 # Get the image for the pod and write it to the file
                 image = pod.spec.containers[0].image
-                # with open(self.filename, "a") as f:
-                    # f.write(image + "\n")
-
+                
                 # Insert the image information into the SQLite database
-                # self.cursor.execute("INSERT INTO images VALUES (?)", (image,))
-                # self.conn.commit()
                 scan_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 # crud.insert_data("images", (image, "2022-01-01 00:00:00"))
-                self.crud.insert_or_ignore_data("images", (image, scan_timestamp))
+                self.crud.insert_or_ignore_data("images", ("", image, scan_timestamp, "", 1))
 
                 # Increase the count and print the progress message
                 self.count += 1
@@ -55,5 +43,5 @@ class ImageCollector:
         print(f"\nFinished. Here are the results:")
         
         # retrieve all data from the images table
-        for image,timestamp in self.crud.select_all("images"):
-            print(f"timestamp: {timestamp}: image: {image}", end="\n")
+        for cluster_name, image, timestamp, registry, amount in self.crud.select_all("images"):
+            print(f"cluster name: {cluster_name} timestamp: {timestamp}: image: {image} registry: {registry} amount: {amount}", end="\n")
