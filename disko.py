b0087b2351f5abc36ed7e6f08076a9a2f15758ed
Daniel Shwartzman <dshwartzman5@gmail.com>
2024-03-21 18:57:29 +0200

    modified disco.py to run the current gui

--- disko.py
+++ disko.py
@@ -1,4 +1,14 @@
-from src.disko.image_collector import ImageCollector
+from src.mvc.view import ImageRegistryManager
 
-collector = ImageCollector()
-collector.collect_images()
+# main function
+def main():
+    # Database file path
+    db_file = 'image_data.db'
+
+    # Run GUI
+    gui = ImageRegistryManager(db_file)
+    gui.run()
+
+# Entry point of the application
+if __name__ == '__main__':
+    main()
