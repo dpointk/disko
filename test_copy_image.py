20d39aa44ca76186c5f436f488a8e58a0283ef48
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-19 22:12:14 +0300

    Test copy images

--- test_copy_image.py
+++ test_copy_image.py
@@ -1,29 +1,18 @@
-# import getpass
-import docker
-from src.disko.image_management.image_controller import *
 from src.disko.image_collector import *
-from src.disko.sqlite import *
+import os
+import docker
 docker_client = docker.from_env()
 
-# # Create db from cluster
-# create_db = ImageCollector().collect_images("kind-cluster1")
-
-# # Import sqlite.py to use any function on my db file
-# sql = SQLiteCRUD("image_data.db")
-
-# # Import image_controller.py to use any function on my db file
+# Import image_controller.py to use any function on my db file
 ctl1 = ImageController("image_data.db")
+password = os.getenv("DOCKERHUB_PASSWORD")
 
-# images = sql.select_all("kind-cluster1")
-# images = ["registry.k8s.io/kube-proxy:v1.25.3"]
 images = ["python:3.8-slim-bullseye"]
 
-# password = getpass.getpass("Enter your Password: ")
-ctl1.copy_images(images, "ygalidan/test1", "3.8-slim-bullseye", "ygalidan", "Ygal3165!")
+ctl1.copy_images(images, "ygalidan/test1", "3.8-slim-bullseye", "ygalidan", password)
 
 def test_copy_images():
-    docker_client.login("ygalidan", "Ygal3165!")
+    docker_client.login("ygalidan", password)
     pull = docker_client.images.pull("ygalidan/test1", "3.8-slim-bullseye")
-
     assert pull is not None, "success"
 
