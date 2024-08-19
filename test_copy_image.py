f29d3cfa0cb66ffc37eb8dd059c591774adda4ae
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-19 20:51:24 +0300

    Test copy images

--- test_copy_image.py
+++ test_copy_image.py
@@ -5,23 +5,23 @@ from src.disko.image_collector import *
 from src.disko.sqlite import *
 docker_client = docker.from_env()
 
-# Create db from cluster
-create_db = ImageCollector().collect_images("kind-cluster1")
+# # Create db from cluster
+# create_db = ImageCollector().collect_images("kind-cluster1")
 
-# Import sqlite.py to use any function on my db file
-sql = SQLiteCRUD("image_data.db")
+# # Import sqlite.py to use any function on my db file
+# sql = SQLiteCRUD("image_data.db")
 
-# Import image_controller.py to use any function on my db file
+# # Import image_controller.py to use any function on my db file
 ctl1 = ImageController("image_data.db")
 
 # images = sql.select_all("kind-cluster1")
-images = ["docker.io/kindest/local-path-provisioner:v0.0.22-kind.0"]
+images = ["python:3.8-slim-bullseye"]
 
 password = getpass.getpass("Enter your Password: ")
-ctl1.copy_images(images, "ygalidan/test1", "v0.0.22-kind.0", "ygalidan", password)
+ctl1.copy_images(images, "ygalidan/test1", "3.8-slim-bullseye", "ygalidan", password)
 
 docker_client.login("ygalidan", password)
-pull = docker_client.images.pull("ygalidan/test1", "v0.0.22-kind.0")
+pull = docker_client.images.pull("ygalidan/test1", "3.8-slim-bullseye")
 
 assert pull is not None, "Success"
 
