4f6d549713e7209b7cc5bd72af16e08e3a36ded0
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-19 21:46:26 +0300

    Test copy images

--- test_copy_image.py
+++ test_copy_image.py
@@ -6,17 +6,17 @@ from src.disko.sqlite import *
 docker_client = docker.from_env()
 
 # # Create db from cluster
-create_db = ImageCollector().collect_images("kind-cluster1")
+# create_db = ImageCollector().collect_images("kind-cluster1")
 
 # # Import sqlite.py to use any function on my db file
-sql = SQLiteCRUD("image_data.db")
+# sql = SQLiteCRUD("image_data.db")
 
 # # Import image_controller.py to use any function on my db file
 ctl1 = ImageController("image_data.db")
 
 # images = sql.select_all("kind-cluster1")
-images = ["registry.k8s.io/kube-proxy:v1.25.3"]
-# images = ["python:3.8-slim-bullseye"]
+# images = ["registry.k8s.io/kube-proxy:v1.25.3"]
+images = ["python:3.8-slim-bullseye"]
 
 password = getpass.getpass("Enter your Password: ")
 ctl1.copy_images(images, "ygalidan/test1", "v1.25.3", "ygalidan", password)
