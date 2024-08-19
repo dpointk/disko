577fca1f0219f6cc11d6e7f27f8f9052ccd7f744
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-19 21:43:50 +0300

    Test copy images

--- test_copy_image.py
+++ test_copy_image.py
@@ -1,4 +1,4 @@
-# import getpass
+import getpass
 import docker
 from src.disko.image_management.image_controller import *
 from src.disko.image_collector import *
@@ -6,22 +6,23 @@ from src.disko.sqlite import *
 docker_client = docker.from_env()
 
 # # Create db from cluster
-# create_db = ImageCollector().collect_images("kind-cluster1")
+create_db = ImageCollector().collect_images("kind-cluster1")
 
 # # Import sqlite.py to use any function on my db file
-# sql = SQLiteCRUD("image_data.db")
+sql = SQLiteCRUD("image_data.db")
 
 # # Import image_controller.py to use any function on my db file
 ctl1 = ImageController("image_data.db")
 
 # images = sql.select_all("kind-cluster1")
-images = ["python:3.8-slim-bullseye"]
+images = ["registry.k8s.io/kube-proxy:v1.25.3"]
+# images = ["python:3.8-slim-bullseye"]
 
-# password = getpass.getpass("Enter your Password: ")
-ctl1.copy_images(images, "ygalidan/test1", "3.8-slim-bullseye", "ygalidan", "Ygal3165!")
+password = getpass.getpass("Enter your Password: ")
+ctl1.copy_images(images, "ygalidan/test1", "v1.25.3", "ygalidan", password)
 
-docker_client.login("ygalidan", "Ygal3165!")
-pull = docker_client.images.pull("ygalidan/test1", "3.8-slim-bullseye")
+docker_client.login("ygalidan", password)
+pull = docker_client.images.pull("ygalidan/test1", "v1.25.3")
 
 assert pull is not None, "Success"
 
