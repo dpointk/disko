0ee41c15341f1ef946afa4c4d3bd4beb00128aae
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-21 20:05:01 +0300

    Provide Unit Test For Copy Images Feature

---
+++ test_copy_image.py
@@ -0,0 +1,17 @@
+from src.disko.image_collector import *
+import os
+import docker
+docker_client = docker.from_env()
+
+# Import image_controller.py to use any function
+ctl1 = ImageController("")
+password = os.getenv("DOCKERHUB_PASSWORD")
+
+images = ["python:3.8-slim-bullseye"]
+
+ctl1.copy_images(images, "ygalidan/test1", "3.8-slim-bullseye", "ygalidan", password)
+
+def test_copy_images():
+    docker_client.login("ygalidan", password)
+    pull = docker_client.images.pull("ygalidan/test1", "3.8-slim-bullseye")
+    assert pull is not None
