addfaf81bdfbcd50e0a498a990fc71d78120c349
gatmbarz123 <bar30301@gmail.com>
2024-08-22 22:50:32 +0300

    UPDATE : Linoy CSS WORK ADD

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Image  from "next/image";
-import Logo from "../../public/assests/Logo.svg";
+import Logo from "../../public/assests/disko.png";
 
 
 const navLinks = [
-    {name: 'Home'},{name : 'About Us'},{name : 'Members'}
+    {name: 'Home'},{name : 'About'}
 ];
 
 export function Navbar() { 
@@ -11,8 +11,7 @@ return(
 <nav className=" bg-body-tertiary justify-beween px-[20px] py[16py] lg:container lg:mx-auto lg:px-10">
   <div className="container-fluid flex items-center justify-between px-4 py-2">
     <div className="flex items-center space-x-3">
-      <Image src={Logo} alt="Logo" width="45" height="40"/> 
-      <span className="ml-2">Disko</span>
+      <Image src={Logo} alt="Logo" width="45" height="40"/>
       <div className="flex gap-x-6">
           {navLinks.map((link, index) => (
             <a className="nav-link" href="/" key={index}>
