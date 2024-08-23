c04b885c2e78486da2cdcf6b5eebb187a6de0b2f
gatmbarz123 <bar30301@gmail.com>
2024-08-23 17:16:11 +0300

    Update API : add a new Button form the Migration

--- backend/new.py
+++ backend/new.py
@@ -1,6 +1,14 @@
 from src.disko.image_management.image_controller import ImageController
+import os
 
-controller = ImageController('')
-image= ["python:3.8-slim-bullseye"]
+os.chdir("/home/gatmbarz123/Desktop/DISKO-P/disko/backend")
 
-controller.copy_images(image,"diskoproject/repo_1.0","3.8-slim-bullseye","diskoproject","barbar13241324")
+controller = ImageController("/home/gatmbarz123/Desktop/DISKO-P/disko/backend/image_data.db")
+
+registry = "diskoproject/repo_1.0"
+tag = "3.8-slim-bullseye" 
+username = "diskoproject"
+password = "barbar13241324"
+helm_chart_path = "/home/gatmbarz123/Desktop/bar"
+
+print(controller.cluster_migration(registry, tag, username, password,helm_chart_path))
