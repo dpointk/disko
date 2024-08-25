fed02ace8fb6b83a032fbde83571a066d5ba986b
keretdodor <keret.dor@gmail.com>
2024-08-25 10:30:58 +0300

    API: code comments added

--- backend/src/disko/image_management/clusters.py
+++ backend/src/disko/image_management/clusters.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, jsonify
 from src.disko.image_management.image_controller import ImageController
 
-# Create a Blueprint for cluster-related routes
+# API created to call the copy_images function to present all local clusters on a machine
+
 cluster_bp = Blueprint('cluster', __name__)
 
 @cluster_bp.route('/api/clusters', methods=['GET'])
 def get_clusters():
+
+     # Initialize the ImageController with the path to the database
     image_controller = ImageController(db_file="src/disko/sqlite.py")
+
+    # Call the get_kubernetes_clusters function
     clusters = image_controller.get_kubernetes_clusters()
     return jsonify(clusters)
