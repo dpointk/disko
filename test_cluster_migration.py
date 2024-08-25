28791d4272456a555613475e4d55053d9424c9a2
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-25 10:11:31 +0300

    Test function cluster_migration

--- test_cluster_migration.py
+++ test_cluster_migration.py
@@ -1,9 +1,8 @@
 from src.disko.image_collector import *
 import getpass
 import os
-
-path = "Octopus/Project_final/disko"
-os.chdir(path)
+import docker
+docker_client = docker.from_env()
 
 # Create db from cluster
 create_db = ImageCollector().collect_images("kind-cluster1")
@@ -12,4 +11,16 @@ create_db = ImageCollector().collect_images("kind-cluster1")
 ctl1 = ImageController("image_data.db")
 
 password = getpass.getpass("Enter your password: ")
-ctl1.cluster_migration("ygalidan/test1", "1", "ygalidan", password, "disko")
+ctl1.cluster_migration("ygalidan/test2", "1", "ygalidan", password, "apphelm")
+yml_values = os.path.join("apphelm/values.yaml")
+image = ctl1.get_current_image(yml_values)[0]
+
+def test_cluster_mirgation():
+    assert image == "ygalidan/test2"
+
+def test_cluster_migration_pull():
+    pull = docker_client.images.pull(image, "1")
+    assert pull is not None
+
+test_cluster_mirgation()
+test_cluster_migration_pull()
