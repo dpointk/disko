775698449d52e2b7d544c546ea4c9d0cfddcf900
keretdodor <keret.dor@gmail.com>
2024-08-25 14:48:48 +0300

    API update: comments added and functionality upgraded

--- backend/api.py
+++ backend/api.py
@@ -17,20 +17,24 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
+
 def get_image_controller():
+    # Initializes and returns an ImageController instance connected to the database
     db_name = os.path.join(base_dir, 'image_data.db')
     print(db_name)
     return ImageController(db_name)
 
-controller =  get_image_controller()
+controller = get_image_controller()
 
 @api.route('/clusters', methods=['GET'])
 def get_clusters():
+    # Fetches a list of Kubernetes clusters and returns it as a JSON response
     clusters = controller.get_kubernetes_clusters()
     return jsonify(clusters)
 
 @api.route('/selected-cluster', methods=['POST'])
 def select_cluster():
+    # Processes the selected cluster by collecting images for it
     data = request.json
     cluster = data.get('cluster')
     
@@ -47,6 +51,7 @@ def select_cluster():
 
 @api.route('/statistics', methods=['GET'])
 def get_statistics():
+    # Retrieves statistical data for a specified cluster and returns it as a JSON response
     cluster = request.args.get('cluster')
     
     if not cluster:
@@ -69,6 +74,7 @@ def get_statistics():
 
 @api.route('/clustermigration',methods=['GET'])
 def migration():
+    # Migrates a cluster's images to a new registry based on provided parameters
     registry = request.args.get('registry')
     tag = request.args.get('tag')
     username = request.args.get('username')
@@ -83,6 +89,7 @@ def migration():
 
 @api.route('/images/<cluster>', methods=['GET'])
 def get_images(cluster):
+    # Fetches the list of images associated with a given cluster and returns it as a JSON response
     try:
         images = controller.present_images_per_cluster(cluster)
         return jsonify(images), 200
@@ -91,30 +98,27 @@ def get_images(cluster):
 
 @api.route('/copyimage', methods=['GET'])
 def copy_images():
-
-    # Extract query parameters from the incoming GET request
-    registry=request.args.get('new_registry')
-    tag=request.args.get('tag')
-    username=request.args.get('target_username')
-    password=request.args.get('target_password')
+    # Copies images to a new registry based on provided parameters
+    registry = request.args.get('new_registry')
+    tag = request.args.get('tag')
+    username = request.args.get('target_username')
+    password = request.args.get('target_password')
     images = request.args.getlist('images')
-
-    # Creating a database file path relative to the base directory
-    db_name = os.path.join(base_dir, 'image_data.db')
-
-     # Initialize the ImageController with the path to the database
-    controller=ImageController(db_name)
+    
     try:
-
         # Call the copy_images method
-        controller.copy_images(images,registry,tag,username,password)
+        controller.copy_images(images, registry, tag, username, password)
 
-          # Return a success message in JSON format if the operation was successful (200) or not (500)
+        # Return a success message in JSON format if the operation was successful (200)
         return jsonify({'message': 'Images copied successfully!'}), 200
     except Exception as e:
+        # Return an error message in JSON format if the operation failed (500)
         return jsonify({'message': str(e)}), 500
  
 
 if __name__ == '__main__':
-    app = create_app()
+    # Configure the Flask app and enable CORS for the API
+    app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    app.register_blueprint(api)
     app.run(debug=True)
