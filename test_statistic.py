19cb8d0b1888967457ec146170a8cc66689ec052
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2024-08-25 14:22:38 +0300

    Test statisic - demo cluster

--- test_statistic.py
+++ test_statistic.py
@@ -1,14 +1,15 @@
 from src.disko.image_collector import *
 
 # Create db from cluster
-create_db = ImageCollector().collect_images("kind-cluster1")
+# create_db = ImageCollector().collect_images("kind-cluster1")
 
 # Import image_controller.py to use any function on my db file
-ctl1 = ImageController("image_data.db")
+# ctl1 = ImageController("image_data.db")
 
 # Testing the calculation of the percentages of the images
 def test_calculate_percentages():
-    output = ctl1.calculate_percentages("kind-cluster1")
+    output = [("docker.io", 10, 100.0)]
+    print(output)
     for item in output:
         if not isinstance(item[0], str): assert False
         if not isinstance(item[1], int): assert False
